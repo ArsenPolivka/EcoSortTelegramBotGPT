a17e7417c8427518dbf289a02f653a78230ee637
unknown <polivka2003@gmail.com>
2023-05-08 20:47:38 +0300

    [tech]: code refactored

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const bot = new TelegramBot(telegramToken, { polling: true });
 bot.onText(/\/start/, (msg) => {
 	bot.sendMessage(
 			msg.chat.id,
-			'Привіт! Я бот, який допоможе вам з утилізацією відходів. Введіть тип відходу, і я надам вам інформацію про сортування.'
+			'Привіт! Я бот, який допоможе вам з утилізацією відходів. ' +
+			'Введіть тип відходу, і я надам вам інформацію про сортування.'
 	);
 });
 
 bot.on('text', async (msg) => {
 	if (msg.text.startsWith('/')) return;
+
 	const sender = `${msg?.chat?.first_name || ''} ${msg?.chat?.last_name || ''}`;
 	const time = `${new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds()}`
 	const wasteType = msg.text;
@@ -24,16 +26,19 @@ bot.on('text', async (msg) => {
 	try {
 		const response = await getSortingInfoFromGPT(wasteType);
 		console.log(`[${time}] ${sender}: ${wasteType}`);
+
 		await bot.sendMessage(msg.chat.id, response);
 	} catch (error) {
 		console.error('Error:', error.message);
+
 		await bot.sendMessage(msg.chat.id, 'An error occurred while retrieving the sorting information. Please try again later.');
 	}
 });
 
 async function getSortingInfoFromGPT(wasteType) {
 	const messages = [
-		{ role: "user", content: `Надай інформацію двома реченнями про те, чи можна сортувати, та куди класти такий тип сміття, як "${wasteType}"`}
+		{ role: "user", content: `Надай інформацію двома реченнями про те, чи можна сортувати,
+		 та куди класти такий тип сміття, як "${wasteType}"`}
 	];
 
 	try {
@@ -53,8 +58,7 @@ async function getSortingInfoFromGPT(wasteType) {
 				}
 		);
 
-		const sortingInfo = response.data.choices[0].message.content.trim();
-		return sortingInfo;
+		return response.data.choices[0].message.content.trim();
 	} catch (error) {
 		throw new Error(`Failed to retrieve sorting information from GPT: ${error.message}`);
 	}
